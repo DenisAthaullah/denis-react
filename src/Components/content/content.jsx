b2159c9707aab63dee88abfcae0bf918034fb088
Denisath <denisathaullahdenis@gmail.com>
2024-09-04 11:17:58 +0700

    commit

--- src/Components/content/content.jsx
+++ src/Components/content/content.jsx
@@ -4,13 +4,13 @@ import './content.css'
 const Content = () => {
     return(
         <div className="container">
-            <img className= "gambar1" src="src/assets/engine.jpg" alt="" />
+            <img className= "gambar1" src="https://i.pinimg.com/564x/db/11/36/db11367ffd068060fb6b55bbd93725ea.jpg " alt="" />
             <p className="mesin">Mesin 4G63T</p>
             <p className="penjelasan">Mesin 4G63T merupakan mesin legendaris dari pabrikan mitsubishi. Mesin tersebut pertama kali digunakan pada mobil mitsubishi Galant VR-4 pada tahun 1988 dan di populerkan oleh mobil mitsubishi Evolution. Mesin tersebut memiliki kapasitas 2000cc 4 silinder segaris dan berkonfigurasi DOHC 16 katup. Mesin tersebut di bekali turbocharged sehingga menghasilkan tenaga sebesar 174hp dan torsi sebesar 253Nm. Tenaga tersebut di raih pada Rpm 4.750. Mesin tersebut adalah salah satu mesin terkencang dan sangat populer pada masa nya.</p>
             <h1>Perbedaan Mesin DOHC dan SOHC</h1>
             <p className="dohc">Perbedaan utama antara mesin SOHC dan DOHC terletak pada jumlah camshaft yang dimiliki. SOHC (Single Overhead Camshaft) hanya memiliki satu camshaft, sedangkan DOHC (Dual Overhead Camshaft) memiliki dua camshaft. 
             </p>
-            <img src="src/assets/dohc sohc.png" alt="" className="gambar2" />
+            <img src="https://assets.promediateknologi.id/crop/0x0:0x0/x/photo/2023/03/08/Screenshot_20230308-1332142-3568123460.png" alt="" className="gambar2" />
             <h2>Mesin SOHC</h2>
             <p>1. Struktur: SOHC memiliki struktur yang lebih sederhana dibandingkan. <br></br>
             2. Keandalan: SOHC lebih mudah dikenali dan diidentifikasi oleh mekanik dan pemilik kendaraan. <br></br>
